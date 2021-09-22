feb935af59875526b3546b1217167dfdf77cc7b9
mehdihasanpour <mehdih761@yahoo.com>
2021-09-22 19:22:04 +0330

    define relation between comment, post and user

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
