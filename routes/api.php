4e95d5554e5a9a4782eccab33c8bb466fa87ce16
pshko <aram.khorsandi@gmail.com>
2021-09-23 19:02:01 +0330

    Auth Logout

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,5 @@ Route::post('/v1/register', [AuthController::class, 'register']);
 Route::post('/v1/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
-
+    Route::post('/v1/logout', [AuthController::class, 'logout']);
 });
