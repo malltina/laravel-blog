39737bb5aac6d5458808c37cb04a7dcdebc0e494
mehdihasanpour <mehdih761@yahoo.com>
2021-09-23 09:38:07 +0330

    add register and login method for users

---
+++ blog/app/Http/Controllers/api/v1/AuthenticationController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticationController extends Controller
+{
+    public function register(Request $request)
+    {
+        $attr = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|unique:users,email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $attr['name'],
+            'password' => bcrypt($attr['password']),
+            'email' => $attr['email']
+        ]);
+
+        $response = [
+            'user'=>$user,
+            'token' => $user->createToken('tokens')->plainTextToken
+        ];
+
+        return response($response, 201);
+    }
+    
+    public function login(Request $request)
+    {
+        $attr = $request->validate([
+            'email' => 'required|string|email|',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if (!Auth::attempt($attr)) {
+            return $this->error('Credentials not match', 401);
+        }
+
+        $user = User::where('email',$attr['email'])->first();
+
+        $response = [
+            'user'=>$user,
+            'token' => $user->createToken('tokens')->plainTextToken
+        ];
+
+        return response($response, 201);
+    }
+
+}
