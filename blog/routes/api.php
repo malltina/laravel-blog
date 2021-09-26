fe28182fe12c659578928466b8e4468b88134eda
mehdihasanpour <mehdih761@yahoo.com>
2021-09-26 10:24:49 +0330

    start testing with test for posts

--- blog/routes/api.php
+++ blog/routes/api.php
@@ -12,13 +12,13 @@ Route::group(['prefix' => 'v1'], function () {
     Route::get('register', [AuthenticationController::class, 'register']);
     Route::post('login', [AuthenticationController::class, 'login']);
 
-    Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::group(['middleware' => 'auth:sanctum','as'=>'posts.'], function () {
 
         Route::post('logout', [AuthenticationController::class, 'logout']);
 
         //Post routes
-        Route::get('posts', [PostController::class, 'index']);
-        Route::post('posts', [PostController::class, 'store']);
+        Route::get('posts', [PostController::class, 'index'])->name('index');
+        Route::post('posts', [PostController::class, 'store'])->name('store');
         Route::get('posts/{post}', [PostController::class, 'show']);
         Route::put('posts/{post}', [PostController::class, 'update']);
         Route::delete('posts/{post}', [PostController::class, 'destroy']);
---
+++ blog/tests/Feature/PostTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    /** @test */
+    public function guest_user_should_not_be_able_to_see_posts()
+    {
+        $tasks = Post::factory(3)->create();
+        $this->getJson(route('posts.index'))
+             ->assertUnauthorized();
+    }
+
+    /** @test */
+    public function only_auht_user_can_create_post()
+    {
+        $user = User::factory()->create();
+        $attributes = Post::factory()->raw();
+        $response = $this->actingAs($user)->postJson(route('posts.store'), $attributes);
+        $attributes['user_id'] = auth()->user()->id;
+        $this->assertDatabaseHas('posts',$attributes);
+
+    }
+}
