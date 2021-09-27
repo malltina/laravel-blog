86bb9140746e828b5ab8c061ea9d11a7c5fee98f
mehdihasanpour <mehdih761@yahoo.com>
2021-09-27 22:26:59 +0330

    add comment factory

--- blog/tests/Feature/CommentTest.php
+++ blog/tests/Feature/CommentTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Comment;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -35,10 +36,7 @@ class CommentTest extends TestCase
     {
         $user = User::factory()->create();
         $post = Post::factory()->create(['user_id'=>$user->id]);
-        $comment = [
-            'body' => 'this is body for comment',
-            'post_id' => $post->id
-        ];
+        $comment = Comment::factory()->raw(['post_id'=>$post->id]);
         $this->actingAs($user)
             ->postJson(route('posts.comment.store', ['post' => $post]), $comment);
         $this->assertDatabaseHas('comments',$comment);
