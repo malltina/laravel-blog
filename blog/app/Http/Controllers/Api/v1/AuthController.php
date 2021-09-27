b8f926f792f9ed73c2d9af760d4a1eb138de1025
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-09-27 19:04:10 +0330

    add controllers and authentication

---
+++ blog/app/Http/Controllers/Api/v1/AuthController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1\Api\v1\Api\v1\Api\v1\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => bcrypt($validated['password']),
+        ]);
+
+        $token = $user->createToken('tkn')->plainTextToken;
+
+        $response = [
+            $user, $token,
+        ];
+        return response($response, 201);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $validated = $request->validated();
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user && !Hash::check($validated['password'], $user->password)) {
+            return response([
+                'message' => 'Bad Request'
+            ], 401);
+        }
+        $token = $user->createToken('tkn')->plainTextToken;
+        $response = [
+            $user, $token
+        ];
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        return [
+            "message" => "logout",
+        ];
+    }
+}
