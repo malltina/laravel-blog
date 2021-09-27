d20c4f790f18cd1ce3e364db94359eb8fa7568e3
pshko <aram.khorsandi@gmail.com>
2021-09-27 20:42:39 +0330

    login tests

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::get('/posts', [PostController::class, 'index']);
     Route::get('/posts/{post}', [PostController::class, 'show']);
 });
