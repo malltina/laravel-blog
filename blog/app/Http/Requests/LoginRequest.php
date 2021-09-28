3a1ae616d69b666604abf981b893393c15f54afa
yasinbaghban <ysnbgn.bgn@gmail.com>
2021-09-28 11:59:37 +0330

    fix request

--- blog/app/Http/Requests/LoginRequest.php
+++ blog/app/Http/Requests/LoginRequest.php
@@ -24,7 +24,7 @@ class LoginRequest extends FormRequest
     public function rules()
     {
         return [
-            'email' => 'required|string|email',
+            'email' => 'required|string',
             'password' => 'required|string|min:8'
         ];
     }
